8cdb33b31b94d28bc737e2f4d546371a5215442b
Elena <Elena@106-01.info2>
2022-11-08 10:37:34 +0100

    DO-WHILE: Ejercicio 5

---
+++ Bucles/src/ejemplos/NumeroAleatorio.java
@@ -0,0 +1,11 @@
+package ejemplos;
+
+public class NumeroAleatorio {
+
+	public static void main(String[] args) {
+		int numeroLista = (int) (Math.random()*30+1);
+		
+		System.out.println("Número de lista: " + numeroLista);
+	}
+
+}
